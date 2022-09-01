207fd0ebfb98a91834ac11f63153f06b75f03ebc
dixitt5 <tilajidixit@gmail.com>
2022-09-01 18:42:43 +0530

    Day-1_Solution Dixit

--- Day1/Day1_Dixit.cpp
+++ Day1/Day1_Dixit.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
 	// int t; cin >> t;
 	// while(t--) {
-		
-		int n; cin >> n;
-		vector <int> v(n);
 
-		for (int i = 0; i < n ; ++i)
-		{
-			cin >> v[i];
-		}
+	int n;
+	cin >> n;
+	vector<int> v(n);
 
-		int cnt = count(v.begin(), v.end(), 0);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v[i];
+	}
 
-		for (int i = 0; i < cnt; ++i)
-		{
-			cout << 0 << " ";
-		}
-		for (int i = 0; i < n-cnt; ++i)
-		{
-			cout << 1 << " ";
-		}
+	int cnt = count(v.begin(), v.end(), 0);
 
-
+	for (int i = 0; i < cnt; ++i)
+	{
+		cout << 0 << " ";
+	}
+	for (int i = 0; i < n - cnt; ++i)
+	{
+		cout << 1 << " ";
+	}
 
 	// }
-return 0;
+	return 0;
 }
