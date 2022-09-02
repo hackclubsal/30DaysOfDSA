eb71a4ad1c29856db160a93ca0c7399bda36a657
Kaushik Lakhani <kaushik.lakhani123@gmail.com>
2022-09-03 00:55:17 +0530

    Update Day2_Kaushik.java

--- Day2/Day2_Kaushik.java
+++ Day2/Day2_Kaushik.java
@@ -15,17 +15,14 @@ public class RotateArray {
         
         return res;
     }
-
 	public static void main(String[] args) {
 		RotateArray ra= new RotateArray();
-		int[] nums= {-1,-100,3,99};
-		int k=2;
+		int[] nums= {1, 2, 3, 4, 5, 6, 7};
+		int k=3;
 		int[] a=ra.rotate(nums, k);
 		
 		for(int x:a) {
 			System.out.println(x);
 		}
-
 	}
-
 }
