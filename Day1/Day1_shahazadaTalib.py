064128bc55c1ba5ab80d7c12c76ef318f5135ae8
shahazadaTalib <50668324+shahazadaTalib@users.noreply.github.com>
2022-09-02 06:39:47 +0530

    Update Day1_shahazadaTalib.py

--- Day1/Day1_shahazadaTalib.py
+++ Day1/Day1_shahazadaTalib.py
@@ -2,28 +2,37 @@
 [1,0,1,0,0,1,1,0]
 
 '''
-
-def sortBinaryArray(arr):
+class SortBinaryArray:
     
-    zeroPtr=0
-    onePtr=len(arr)-1
-    while(onePtr>zeroPtr):
-        if(arr[zeroPtr]==0):
-            zeroPtr+=1
-        if(arr[onePtr]==1):
-            onePtr-=1
-        else:
-            arr[zeroPtr],arr[onePtr]=arr[onePtr],arr[zeroPtr]
+    def __init__(self,arr):
+        self.arr = arr
+        self.n = len(arr)
+
+    def sortBinaryArray(self):
+        zeroPtr=0
+        onePtr=len(arr)-1
+        while(onePtr>zeroPtr):
+            if(arr[zeroPtr]==0):
+                zeroPtr+=1
+            if(arr[onePtr]==1):
+                onePtr-=1
+            else:
+                arr[zeroPtr],arr[onePtr]=arr[onePtr],arr[zeroPtr]
+     
+    def displayArray(self):
+        print(self.arr)
     
         
 
 if __name__=="__main__":
     
-    inpt=input("anter array: ")
+#     inpt=input("anter array: ")
+#     arr=[int(i) for i in inpt.split(',')]
 
-    arr=[int(i) for i in inpt.split()]
-    sortBinaryArray(arr)
-    print(arr)
+    arr = [1, 0, 1, 0, 1, 0, 0, 1]
+    obj = SortBinaryArray(arr)
+    obj.sortBinaryArray()
+    obj.displayArray()
 
 
 
