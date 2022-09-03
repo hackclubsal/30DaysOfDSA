dc3db30dc36a2fc1a5fc1c2390ff6a47c3dc92db
VaishaliLimje1806 <vaishulimje1806@gmail.com>
2022-09-03 20:45:59 +0530

    Day 2 Final Code

--- Day2/day2-Vaishali.cpp
+++ Day2/day2-Vaishali.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void rotate(int arr[], int k, int n)
-{
-    k = k%n;
-    
-    for(int i=0; i<n; i++)
-    {
-        if(i<k)
+void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        int low=n-k, high=n-1;
+        while(low<high)
         {
-            cout<<arr[n+i-k]<<" ";
+            swap(nums[low++], nums[high--]);
         }
-        else
-            cout<<arr[i-k]<<" ";
+        
+        int low1=0, high1=n-k-1;
+        while(low1<high1)
+        {
+            swap(nums[low1++], nums[high1--]);
+        }
+        
+        int low2=0, high2=n-1;
+        while(low2<high2)
+        {
+            swap(nums[low2++], nums[high2--]);
+        }
     }
-}
 
 int main()
 {
-    int n,k;
+    int n,k,a;
     cin>>n;
-    int arr[n];
+    vector<int> nums;
     for(int i=0; i<n; i++)
-        cin>>arr[i];
+    {
+        cin>>a;
+        nums.push_back(a);
+    }    
 
     cin>>k;
 
-    rotate(arr, k, n);
+    rotate(nums, k);
+
+    for(auto x : nums)
+        cout<<x<<" ";
 
     return 0;
 }
