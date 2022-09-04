6493b914ea644d14a16d49b7e951cd6cff87c141
Jay Rathod <86549654+Jayy31@users.noreply.github.com>
2022-09-04 19:49:14 +0530

    Added Day4 solution

---
+++ Day 4/Day4_JayRathod.java
@@ -0,0 +1,25 @@
+
+public class Day4_JayRathod {
+    public static void main(String[] args){
+        System.out.println(isPerfectSquare(25));
+    }
+    static boolean isPerfectSquare(int num) {
+        long start=0;
+        long end=num;
+        while(start<=end){
+            long mid=start+(end-start)/2;
+            long val=mid*mid;
+            if(val==num){
+                return true;
+            }
+            else if(val>num){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return false;
+    }
+}
+
