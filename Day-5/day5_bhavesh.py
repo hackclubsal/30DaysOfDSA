2e917387c2ddaff47825171dc9386a8c89d24999
Bhavesh Patel <86740153+uranium2037@users.noreply.github.com>
2022-09-05 16:04:51 +0530

    Create day5_bhavesh.py

---
+++ Day-5/day5_bhavesh.py
@@ -0,0 +1,13 @@
+def search(arr, n):
+  ans = -1
+  for i in range(0,n,2):
+    ans = arr[i]
+    break
+ if(arr [n-2] != arr[n-1]):
+  ans = arr[n-1]
+print("Its", ans)
+  
+arr = [1,1,2,3,3,4,4]
+Len = len(arr)
+
+search(arr, Len)
