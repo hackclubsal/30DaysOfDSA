afc4842844f44600eb6c5b0f119a6b9da48d79cb
Vishvesh Fumakiya <68076050+Vishvesh001@users.noreply.github.com>
2022-09-20 23:06:22 +0530

    Day19_Solution_VishveshFumakiya.py

---
+++ Day - 19/Day19_VishveshFumakiya.py
@@ -0,0 +1,15 @@
+#Determine if S can be broken down into a sequence of words where each word is an element in W
+def wordCheck(w, s):
+    if not s:
+        return True
+    for i in range(1, len(s) + 1):
+        prefix = s[:i]
+        if prefix in w and wordCheck(w, s[i:]):
+            return True
+    return False       
+
+w = ['tech', 'work', 'problem', 'at']
+s = 'workattech'
+
+res = wordCheck(w, s)
+print(res)
