fbb838c932b87a594f7d84d713598d99685acacc
jayy1401 <112607680+jayy1401@users.noreply.github.com>
2022-09-25 10:28:30 +0530

    Create #day8_jay.py

---
+++ Day-8/#day8_jay.py
@@ -0,0 +1,19 @@
+class MaxSubArray:
+    def __init__(self, array):
+        self.array = array
+        self.max_sum = 0
+        self.max_subarray = []
+
+    def subarray(self):
+        for i in range(len(self.array)):
+            for j in range(i, len(self.array)):
+                print(self.array[i:j+1])
+                if sum(self.array[i:j+1]) > self.max_sum:
+                    self.max_sum = sum(self.array[i:j+1])
+
+        return self.max_sum
+
+if __name__ == "__main__":
+    array = [-2, -3, 4, -1, -2, 1, 5, -3]
+    max_subarray = MaxSubArray(array)
+    print(max_subarray.subarray())
